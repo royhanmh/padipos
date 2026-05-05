580a99dd5314b71fd5686e53b13e9ccb815f3530
Muhammad Zaynurroyhan <royhanm35@gmail.com>
2026-05-05 22:18:00 +0700

    style(auth): login cards logo resize

--- frontend/src/components/LoginCardComponent.jsx
+++ frontend/src/components/LoginCardComponent.jsx
@@ -12,19 +12,19 @@ const LoginCardComponent = ({
     >
       <div className="text-center">
         <img
-          className="mx-auto h-36 w-36 object-contain max-lg:h-28 max-lg:w-28"
+          className="mx-auto h-40 w-40 object-contain max-lg:h-40 max-lg:w-40"
           src={logoSrc}
           alt={logoAlt}
         />
         <h1 className="mb-2.5 text-4xl font-semibold text-[#2B2B2B] max-lg:text-[34px]">
           {title}
         </h1>
-        <p className="text-[19px] text-[#919191] max-lg:text-[16px]">{subtitle}</p>
+        <p className="text-[19px] text-[#919191] max-lg:text-[16px]">
+          {subtitle}
+        </p>
       </div>
 
-      <div className="mt-14 px-6 max-lg:mt-9 max-lg:px-3">
-        {children}
-      </div>
+      <div className="mt-14 px-6 max-lg:mt-9 max-lg:px-3">{children}</div>
     </div>
   );
 };
